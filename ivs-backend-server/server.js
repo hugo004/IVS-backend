6339b039a8819c44b04e3d2d861f7b05403049a4
Hugo <hugo444312@gmail.com>
2019-07-12 23:37:38 +0800

    add / response

--- ivs-backend-server/server.js
+++ ivs-backend-server/server.js
@@ -29,12 +29,15 @@ app.use(fileUpload());
 require('events').EventEmitter.defaultMaxListeners = 100;
 
 
+app.get('/', function (req, res) {
+  res.send('IVS Server Running')
+})
 
-var server = app.listen(8081, function () {
+var server = app.listen(8081, '0.0.0.0', function () {
   var host = server.address().address
   var port = server.address().port
   
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Example app listening at http://%s:%s %s", host, port, process.env.NODE_ENV)
 })
 const jwt = require('jsonwebtoken');
 const userCardPool = new Map();
@@ -44,79 +47,5 @@ require('./API/admin.js')(app, jwt, NS, userCardPool);
 require('./API/user.js')(app, jwt, NS, userCardPool);
 
 
-//listene event emitted from network
-Network.connection.on('event', async event => {
-
-  //check event type 
-
-  //if request access user's asset event
-
-  //emit event to target user for asset request
-  const eventInfo = {
-    type: event.getType(),
-    identifier: event.getIdentifier(),
-    eventId: event.eventId,
-    timestamp: event.timestamp,
-    conent: event.detail,
-    transaction: event.transaction
-  };
-  console.log('received event')
-  console.log(eventInfo);
-
-  // const definition = await Network.connect();
-  // const factory = definition.getFactory();
-  // const transaction = factory.newTransaction('org.example.ivsnetwork', eventInfo.transaction);
-  // transaction.param = 'change';
-});
-
-
-
-
-/**
- * to check network is online
- */
-app.post('/api/ping', async function (req, res) {
-  try {
-    //init the network
-    await Network.connect();
-
-    //ping network
-    let result = await Network.ping();
-
-    //output network info
-    res.json({"user": result});
-  }
-  catch (error) {
-    res.status(500).json({"error": error.toString()});
-  }
-})
-
-
-
-
-
-app.post('/api/eventTest', async function(req, res) {
-  try {
-    //get defined participant from network
-    const definition = await Network.connect();
-    const connection = Network.getConnection();
-    
-    const factory = definition.getFactory()
-    const transaction = factory.newTransaction('org.example.ivsnetwork', 'TestTransaction');
-    transaction.param = 'test';
-    await connection.submitTransaction(transaction);
-
-    //return the result
-    res.status(200).json({
-      result: 'transaction submitted'
-    });
-      
-  }
-  catch (error) {
-    res.status(500).json({
-      error: error.toString()
-    });
-  }
-})
 
 
